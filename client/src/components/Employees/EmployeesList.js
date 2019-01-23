ada73c9fbcad2aa14a37f01f5aed8133bfb80f45
crmgunter <crmgunter@gmail.com>
2019-01-23 17:35:41 -0500

    random emails generate and post to database

--- client/src/components/Employees/EmployeesList.js
+++ client/src/components/Employees/EmployeesList.js
@@ -8,9 +8,22 @@ const Header = styled.div`
     justify-content: space-around;
 `
 
+const Table = styled.table`
+    border: 1px solid black;
+
+    td {
+        border: 1px solid black;
+        padding: 10px;
+    }
+    td:nth-child(odd) {
+        background: lightblue;
+    }
+`
+
 class EmployeesList extends Component {
     state = {
         employees: [{
+            email: '',
             children: [{}]
         }]
     }
@@ -26,7 +39,7 @@ class EmployeesList extends Component {
 
     deleteEmployee = (employeeId) => {
         axios.delete(`/api/employees/${employeeId}`)
-        .then(() => this.getAllEmployees())
+            .then(() => this.getAllEmployees())
     }
 
     generateEmail = (firstName, lastName) => {
@@ -42,53 +55,66 @@ class EmployeesList extends Component {
             const getRandomNumbers = numbers.charAt(Math.floor(Math.random() * numbers.length))
             randomNumbers += getRandomNumbers
         }
-        console.log(firstName, randomNumbers, lastName, randomLetters)
         const newEmail = `${firstName}${lastName}${randomNumbers}${randomLetters}@heliumservices.com`
-        console.log(newEmail)
+        return newEmail
     }
 
-    createNewEmail = () => {
+    createNewEmail = (event) => {
+        event.preventDefault()
+        const newState = []
         this.state.employees.map((employee) => {
-            this.generateEmail(employee.firstName, employee.lastName)
+            const generatedEmail = this.generateEmail(employee.firstName, employee.lastName)
+            console.log(generatedEmail)
+            employee.email = generatedEmail
+            console.log(employee)
+            newState.push(employee)
+            axios.patch(`/api/employees/${employee._id}`, employee)
+                .then(() => {
+                    this.getAllEmployees()
+                })
         })
+        this.setState({ employees: newState })
     }
 
     render() {
-        
         return (
             <div>
                 <Header>
                     <h1>Employees</h1>
-                    <button>Generate Emails</button>
+                    <div>
+                    <button onClick={this.createNewEmail}>Generate Emails</button>
                     <Link to="/employees/new">
-                    <button>Add Employee</button>
+                        <button>Add Employee</button>
                     </Link>
+                    </div>
                 </Header>
-                <table>
+                <Table>
                     <tr>
                         <td>EmployeeId</td>
                         <td>First Name</td>
                         <td>First Name</td>
                         <td>Birth Date</td>
                         <td>Job Title</td>
+                        <td>Email</td>
                         <td>Number of Children</td>
                         <td>Delete</td>
                     </tr>
                     {this.state.employees.map((employee, i) => (
-                        
+
                         <tr key={i}>
                             <td>{employee._id}</td>
                             <Link to={`/employees/${employee._id}`}><td>{employee.firstName}</td></Link>
                             <td>{employee.lastName}</td>
                             <td>{employee.birthDate}</td>
                             <td>{employee.title}</td>
+                            <td>{employee.email}</td>
                             <td>{employee.children.length}</td>
                             <td>
                                 <button onClick={() => this.deleteEmployee(employee._id)}>Delete</button>
                             </td>
                         </tr>
-                ))}
-                </table>
+                    ))}
+                </Table>
             </div>
         );
     }
